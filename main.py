a6e60d07d0cfe909a105d5c3b6fae272153204b4
213181712 <62132666+213181712@users.noreply.github.com>
2026-01-07 20:59:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+from image_utils import load_image, edge_detection
 
+flower = load_image('/content/flower.jpg')
+plt.imshow(flower)
+plt.axis('off')
+plt.show()
+
+from skimage.filters import median
+from skimage.morphology import ball
+
+clean_image = median(flower, ball(3))
+
+edge_test = edge_detection(clean_image)
+edge_binary = edge_test > 100
+edge_image = Image.fromarray(edge_binary)
+edge_image.save('my_edges.png')
+
+
+plt.imshow(edge_binary, cmap='gray')
